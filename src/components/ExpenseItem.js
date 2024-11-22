7eefed4db34d5f4e8093df903a1d32391082a1ad
RV <rauno.valk@voco.ee>
2024-11-22 11:32:25 +0200

    Utilizing props and arrays

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,12 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
 
-    console.log(props)
-    console.log(props.dataname)
-
     return(
         <div className='expense-item'>
-            <div>{props.dataname.date.toString()}</div>
-                <div className='expense-item__description'>
+            <ExpenseDate date={props.dataname.date}/>  
+            <div className='expense-item__description'>
                 <h2>{props.dataname.title}</h2>
                 <div className='expense-item__price '>{props.dataname.price}</div>
             </div>
