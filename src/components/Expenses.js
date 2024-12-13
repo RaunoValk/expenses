f36da6fd70801cd69cbfb7e78b7a6711ea930a62
RV <rauno.valk@voco.ee>
2024-12-13 15:37:47 +0200

    Added Card

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,13 +1,14 @@
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import Card from './Card';
 
 const Expenses = (props) => {
 
     return(
-        <div className="expenses">
+        <Card className="expenses">
             <ExpenseItem data={props.expenses[0]}/>
             <ExpenseItem data={props.expenses[1]}/>
-    </div>
+    </Card>
 );
 } 
 export default Expenses;
